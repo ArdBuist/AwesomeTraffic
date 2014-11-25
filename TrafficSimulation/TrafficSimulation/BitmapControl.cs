f41e64e0329fed9cb46c34c489739115517bc0d8
Ard Buist <ard1995@gmail.com>
2014-11-25 10:08:13 +0100

    terug naar 4 dagen geleden na probleempjes

--- TrafficSimulation/TrafficSimulation/BitmapControl.cs
+++ TrafficSimulation/TrafficSimulation/BitmapControl.cs
@@ -3,46 +3,16 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Resources;
 using System.Threading.Tasks;
 
 namespace TrafficSimulation
 {
     class BitmapControl
     {
-<<<<<<< HEAD
-        public BitmapControl(Size size)
-        {
-        }
+        Bitmap kaart;
         private void DrawBitmap()
         {
-            
-        }
-        public void Teken(Graphics gr)
-        {
-        }
-        public void AddTile(Graphics gr,string tileName, int x,int y, SimControl control)
-        {
-            ITile[] tileklasses = new ITile[] {new Crossroad, new Fork, new Road,new CurvedRoad };
 
-            ITile tile;
-            int i;
-            while(tile == null)
-            {
-                ITile tileklasse = tileklasses[i];
-                if(tileklasse.ToString() == tileName)
-                    tile = tileklasse;
-                i++;
-            }
-            control.tiles.Add(tile);
-            //afbeelding uit de resources halen en in de grid op een plek invoegen.
-            ResourceManager rm = Properties.Resources.ResourceManager;
-            Bitmap tile = (Bitmap)rm.GetObject(tileName);
-            gr.DrawImage(tile, x*100, y*100);
         }
-=======
-        Bitmap kaart;
-        private void DrawBitmap();
->>>>>>> parent of fb9b762... Klassen aangepast zodat programma uitgevoerd wordt.
     }
 }
--- TrafficSimulation/TrafficSimulation/SimControl.cs
+++ TrafficSimulation/TrafficSimulation/SimControl.cs
@@ -15,26 +15,10 @@ namespace TrafficSimulation
         BuildPanel buildPanel;
         ControlPanel controlPanel;
         ITile currentTile;
-        BitmapControl bitmap;
-        public List<ITile> tiles;
 
-        public SimControl(Size size)
+        public SimControl()
         {
-            tiles = new List<ITile>();
-            bitmap = new BitmapControl(size);
-            this.Size = new Size(1000,1000);
-            this.Paint += this.Teken;
-            this.Visible = true;
-            Invalidate();
-        }
-        private void Teken(object o, PaintEventArgs pea)
-        {
-            //Dit moet bij een betere interface anders worden aangeroepen.
-            bitmap.AddTile(pea.Graphics, "crossroad", 5, 5,this);
-            bitmap.AddTile(pea.Graphics, "crossroad", 3, 3, this);
-            bitmap.AddTile(pea.Graphics, "horizontalroad", 6, 5, this);
-            bitmap.AddTile(pea.Graphics, "horizontalroad", 4, 5, this);
-            bitmap.AddTile(pea.Graphics, "horizontalroad", 6, 3, this);
+
         }
     }
 }
