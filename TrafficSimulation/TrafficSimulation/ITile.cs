0c2a0bb0a0e35a2bb2f309b656b656b90c2c2b99
maaikemaas <maaikemaas@msn.com>
2014-11-27 00:24:23 +0100

    tekenen normaal kruispunt
    de methode drawcrossroad is zo aangepast dat er kruispunt worden
    getekend m.b.v. input over de wegen in die en uit gaan bij elke zijde.
    er wordt een graphics-object gereturnd.

--- TrafficSimulation/TrafficSimulation/ITile.cs
+++ TrafficSimulation/TrafficSimulation/ITile.cs
@@ -25,7 +25,7 @@ namespace TrafficSimulation
         bool[] acces;
         int TotalVehicleLength;
         Size size;
-        Bitmap image;
+        Bitmap image = new Bitmap(100,100);
 
         public virtual void RemoveVehicle()
         {
@@ -51,6 +51,42 @@ namespace TrafficSimulation
         {
             tc = new TrafficlightControl();
         }
+        
+        //Hier wordt het kruispunt getekend m.b.v. parameters die aangeven hoeveel wegen er in en uit gaan bij elke zijde.
+        public Graphics drawCrossroad(int upIn, int upOut, int rightIn, int rightOut, int downIn, int downOut, int leftIn, int leftOut)
+        {
+            Graphics crossRoad = BitmapGraphics();
+
+            // Er worden vier lijnen getekend: lineRU hoort bij de lijn rechtsboven, lineRD bij rechtsonder, lineLD bij linksonder, lineLU bij linksboven
+             int lineRUx      = 50 + (10 * upOut);
+             int lineRUy      = -1 * (50 - (10 * rightIn));
+             int lineRUheight = 2 * (50 - (10 * rightIn));
+             int lineRUwidth  = 2 * (50 - (10 * upOut));
+             
+             int lineRDx      = 50 + (10 * downIn);
+             int lineRDy      = 50 + (10 * rightOut);
+             int lineRDheight = 2 * (50 - (10 * rightOut));
+             int lineRDwidth  = 2 * (50 - (10 * downIn));
+             
+             int lineLDx      = -1 * (50 - (10 * downOut));
+             int lineLDy      = 50 + (10 * leftIn);
+             int lineLDheight = 2 * (50 - (10 * leftIn));
+             int lineLDwidth  = 2 * (50 - (10 * downOut));
+              
+             int lineLUx      = -1 * (50 - (10 * upIn));
+             int lineLUy      = -1 * (50 - (10 * leftOut));
+             int lineLUheight = 2 * (50 - (10 * leftOut));
+             int lineLUwidth  = 2 * (50 - (10 * upIn));
+                        
+            crossRoad.DrawArc(Pens.Black,lineRUx,lineRUy,lineRUwidth,lineRUheight, 90,90);
+            crossRoad.DrawArc(Pens.Black,lineRDx,lineRDy,lineRDwidth,lineRDheight, 180,90);
+            crossRoad.DrawArc(Pens.Black,lineLDx,lineLDy,lineLDwidth,lineLDheight, 270,90);
+            crossRoad.DrawArc(Pens.Black,lineLUx,lineLUy,lineLUwidth,lineLUheight, 0,90);
+
+            return crossRoad;
+        }
+        
+
         public override void RemoveVehicle()
         {
             base.RemoveVehicle();
@@ -75,6 +111,7 @@ namespace TrafficSimulation
     }
     public class Road: Roads
     {
+
         public override string ToString() { return "road"; }
         int startDirection;
         int eindDirection;
