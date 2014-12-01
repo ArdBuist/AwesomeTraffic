182d49b5014b1922d8c14aab60a0dc20f5b63e91
Fortunos <a.leestemaker@students.uu.nl>
2014-12-01 13:51:35 +0100

    de trafficlightMap in trafficlight proberen te krijgen

--- TrafficSimulation/TrafficSimulation/Trafficlight.cs
+++ TrafficSimulation/TrafficSimulation/Trafficlight.cs
@@ -9,28 +9,27 @@ namespace TrafficSimulation
 {
     class Trafficlight
     {
-        int color;
+        Color color;
         Point Position;
         Crossroad road;
+
+
         public Trafficlight(Crossroad road,Point Position)
         {
             this.road = road;
             this.Position = Position;
             DrawTrafficlight(Color.Red);
         }
+
         public void UpdateColor(Color kleur)
         {
             //hierin kunnen nog meer acties worden gedaan als de kleur wordt veranderd, zoals andere positie van de lichten t.o.v. elkaar.
             DrawTrafficlight(kleur);
+            color = kleur;
         }
         public void DrawTrafficlight(Color kleur)
         {
-            Graphics gr = road.Graphics;
-            //de road heeft nog geen Graphics, dus stoplicht wordt nog niet getekend.
-            gr.DrawEllipse(new Pen(kleur), Position.X,Position.Y, 10, 10);
-
-
-
+            
         }
     }
 }
