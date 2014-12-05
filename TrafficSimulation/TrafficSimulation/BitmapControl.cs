d67cf7c894018dc9e94c10efde74da5f11bccff3
marcopoloq <markheinsbroek@gmail.com>
2014-12-05 14:14:47 +0100

    foutje
    niks verandert

--- TrafficSimulation/TrafficSimulation/BitmapControl.cs
+++ TrafficSimulation/TrafficSimulation/BitmapControl.cs
@@ -11,26 +11,63 @@ namespace TrafficSimulation
 {
     public class BitmapControl
     {
-
         public Bitmap bitmap;
+
         public BitmapControl(Size size)
         {
-            bitmap = new Bitmap(size.Width,size.Height);
-            
+            bitmap = new Bitmap(size.Width, size.Height);
+
         }
         // altijd aanroepen als er in de bitmap getekend moet worden.
         public Graphics GetBitmapGraphics
         {
             get { return Graphics.FromImage(bitmap); }
         }
+
         private void DrawBitmap()
         {
-            
+
         }
-        public void AddTile(Bitmap bitmap, int x, int y)
-        {//zorgt ervoor dat de nieuwe tile op de kaart komt in het grid.
-            Graphics gr = GetBitmapGraphics;
-            gr.DrawImage(bitmap, x*100, y*100);
+
+        public void addVehicle()
+        {
+
+        }
+
+        public void updateBitmap()
+        {
+
         }
     }
+
+    class tileBitmapControl : BitmapControl
+    {
+        public tileBitmapControl(Size size)
+            : base(size)
+        {
+
+        }
+
+        public void AddObject(Bitmap bitmap, int x, int y)
+        {//zorgt ervoor dat de nieuwe tile op de kaart komt in het grid.
+            Graphics gr = GetBitmapGraphics;
+            gr.DrawImage(bitmap, x * 100, y * 100);
+        }
+    }
+
+    class vehicleBitmapControl : BitmapControl
+    {
+        public vehicleBitmapControl(Size size)
+            : base(size)
+        {
+
+        }
+
+        public void AddObject(Bitmap bitmap, int x, int y)
+        {
+            Graphics gr = GetBitmapGraphics;
+            gr.DrawImage(bitmap, x, y);
+        }
+    }
+
 }
