e1c758a094774208de2f0715e29b5d47465ceb07
maaikemaas <maaikemaas@msn.com>
2014-12-29 23:21:12 +0100

    vervanging variabelen klikmethodes
    nu is er 1 variabele die aangeeft welke knop er is aangeklikt, ipv voor
    elke methode een aparte variabele

--- TrafficSimulation/TrafficSimulation/ExtraButtonsOS.xaml.cs
+++ TrafficSimulation/TrafficSimulation/ExtraButtonsOS.xaml.cs
@@ -41,54 +41,42 @@ namespace TrafficSimulation
         //klikmethode rechte weg 1 naar 3 (verticaal)
         private void Roadver_Checked(object sender, RoutedEventArgs e)
         {
-            s.eraser = false;
-            s.selected = false;
-            s.building = true;
+            s.state = "building";
             s.currentBuildTile = new Road(1, 3);
         }
 
         //klikmethode rechte weg 2 naar 4 (horizontaal)
         private void Roadhor_Checked(object sender, RoutedEventArgs e)
         {
-            s.eraser = false;
-            s.selected = false;
-            s.building = true;
+            s.state = "building";
             s.currentBuildTile = new Road(2, 4);
         }
 
         //klikmethode bocht 1 naar 2
         private void Curve12_Checked(object sender, RoutedEventArgs e)
         {
-            s.eraser = false;
-            s.selected = false;
-            s.building = true;
+            s.state = "building";
             s.currentBuildTile = new Road(1, 2);
         }
 
         //klikmethode bocht 2 naar 3
         private void Curve23_Checked(object sender, RoutedEventArgs e)
         {
-            s.eraser = false;
-            s.selected = false;
-            s.building = true;
+            s.state = "building";
             s.currentBuildTile = new Road(2, 3);
         }
 
         //klikmethode bocht 3 naar 4
         private void Curve34_Checked(object sender, RoutedEventArgs e)
         {
-            s.eraser = false;
-            s.selected = false;
-            s.building = true;
+            s.state = "building";
             s.currentBuildTile = new Road(3, 4);
         }
 
         //klikmethode bocht 1 naar 4
         private void Curve14_Checked(object sender, RoutedEventArgs e)
         {
-            s.eraser = false;
-            s.selected = false;
-            s.building = true;
+            s.state = "building";
             s.currentBuildTile = new Road(1, 4);
         }
 
